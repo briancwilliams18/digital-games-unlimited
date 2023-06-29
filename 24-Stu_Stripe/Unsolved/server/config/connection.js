5eab56d723d7ea0a6eb790639f5243834a5f2053
Destroer47 <andrewj5787@jesterz.net>
2023-06-29 19:42:03 -0400

    temp

--- 24-Stu_Stripe/Unsolved/server/config/connection.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/mern-shopping', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-
-module.exports = mongoose.connection;
