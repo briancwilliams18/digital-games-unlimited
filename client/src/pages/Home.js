1d917add601060037729e2a33642fd2526171332
Destroer47 <andrewj5787@jesterz.net>
2023-07-06 18:56:11 -0400

    working save

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -3,6 +3,10 @@ import ProductList from "../components/ProductList";
 import CategoryMenu from "../components/CategoryMenu";
 import Cart from "../components/Cart";
 
+const apiKey = "e9504ab12bda4519a04e845996b47b09";
+
+const queryURL = "https://api.rawg.io/api/games" + "&appid=" + apiKey;
+
 const Home = () => {
   return (
     <div className="container">
@@ -12,5 +16,7 @@ const Home = () => {
     </div>
   );
 };
+
+console.log(fetch(queryURL));
 
 export default Home;
